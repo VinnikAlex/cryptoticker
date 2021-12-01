7cf358316039089dbbbd279aff09a8a0648e195f
Alexander Vinnik <VinnikAlex0@gmail.com>
2021-12-01 16:26:41 +1100

    added show more option to display top 100 coins

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let grid = document.querySelector("#crypto-grid");
-let coins = 24;
+let coins = 10;
 
+let showMore = document.querySelector("#show-more");
+
+// load top 10 marketcap coins
 requestApi();
 
 function requestApi() {
@@ -18,15 +21,24 @@ function cryptoDetails(info) {
   // specify how many rows are required:
   // number of crypto coins / 4 rows
   let numColumns = info.length / 4;
-  // specify number of columns dependant on how many coins are showing
+  // specify number of columns depending on how many coins are showing
   grid.style.gridTemplateRows = `repeat(${numColumns}, 1fr)`;
 
   // add the name to the cryptoName div
   for (let i = 0; i < info.length; i++) {
     document.querySelector("#cryptoName" + [i]).innerHTML +=
       i + 1 + "." + " " + info[i].symbol;
-
+    // add the image to the cryptoImage div
     document.querySelector("#cryptoImage" + [i]).src = info[i].image;
+    // add the price to the cryptoPrice div
+    document.querySelector("#cryptoPrice" + [i]).innerHTML =
+      "Price:" + " " + "$" + info[i].current_price;
+    // add the marketcap to the cryptoCap div
+    document.querySelector("#cryptoCap" + [i]).innerHTML =
+      "Market Cap:" + " " + info[i].market_cap;
+    // add the daily change to the cryptoChange div
+    document.querySelector("#cryptoChange" + [i]).innerHTML =
+      "Daily Change:" + " " + info[i].price_change_percentage_24h;
   }
 }
 
@@ -44,10 +56,37 @@ function addDivs(info) {
     nameHeading.setAttribute("id", "cryptoName" + i);
     div.appendChild(nameHeading);
 
+    //create an img tag for crptocoin image
     let cryptoImage = document.createElement("img");
     cryptoImage.setAttribute("id", "cryptoImage" + i);
     div.appendChild(cryptoImage);
+
+    //create p tag for crypto price
+    let cryptoPrice = document.createElement("p");
+    cryptoPrice.setAttribute("id", "cryptoPrice" + i);
+    div.appendChild(cryptoPrice);
+
+    //create p tag for crypto marketcap
+    let cryptoCap = document.createElement("p");
+    cryptoCap.setAttribute("id", "cryptoCap" + i);
+    div.appendChild(cryptoCap);
+
+    //create p tag for 24h daily change (percentage)
+    let cryptoChange = document.createElement("p");
+    cryptoChange.setAttribute("id", "cryptoChange" + i);
+    div.appendChild(cryptoChange);
   }
+  removeElements(info);
+}
+
+function removeElements(info) {
+  showMore.addEventListener("click", (e) => {
+    for (let i = 0; i < info.length; i++) {
+      grid.removeChild(grid.childNodes[0]);
+    }
+    coins = 100;
+    requestApi();
+  });
 }
 
 // function addContent(index) {
